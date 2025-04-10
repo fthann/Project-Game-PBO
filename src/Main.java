dfa77d4f29389d733032d6f2dbb0c7a25e772190
fthann <fathanalfrq@gmail.com>
2025-04-10 13:08:06 +0700

    final changes

--- src/Main.java
+++ src/Main.java
@@ -7,54 +7,68 @@ public class Main {
 
         System.out.print("Masukkan nama karaktermu: ");
         String name = input.nextLine();
-        
+
         System.out.print("Masukkan umur karaktermu: ");
         int age = input.nextInt();
-        input.nextLine(); 
-        
+        input.nextLine();
+
         System.out.print("Masukkan gender karaktermu (Laki-laki/Perempuan): ");
         String gender = input.nextLine();
 
         Character player = new Character(name, age, gender, 100);
 
-        // Membuat 10 scene
-        Scene planetScene = new Scene("Kamu berhasil menemukan planet layak huni!", 
-            "Mendarat", null, 0, 50, 
-            "Lewati", null, 10, 10, 
-            "Kembali", null, 5, 5
-        );
+        // Membuat 10 scene (dari akhir ke awal)
+        Scene scene10 = new Scene("Kamu berhasil mendarat di planet baru yang layak huni.",
+            "Mulai hidup baru", null, 0, 50,
+            "Jelajahi planet", null, 5, 10,
+            "Kirim sinyal ke bumi", null, 0, 5);
 
-        Scene blackHoleScene = new Scene("Kamu hampir tertarik ke lubang hitam!", 
-            "Gunakan mesin dorong", planetScene, 5, 10, 
-            "Gunakan gravitasi", planetScene, 10, 15, 
-            "Abaikan peringatan", planetScene, 20, 5
-        );
+        Scene scene9 = new Scene("Planet ditemukan, tapi penuh bahaya.",
+            "Scan lingkungan", scene10, 0, 10,
+            "Mendarat cepat", scene10, 10, 5,
+            "Cari planet lain", scene10, 5, 0);
 
-        Scene asteroidScene = new Scene("Hujan asteroid mendekat!", 
-            "Hindari dengan kecepatan tinggi", blackHoleScene, 5, 10, 
-            "Gunakan perisai", blackHoleScene, 0, 5, 
-            "Terobos", blackHoleScene, 15, 0
-        );
+        Scene scene8 = new Scene("Persediaan menipis.",
+            "Cek kompartemen cadangan", scene9, 0, 10,
+            "Minta bantuan dari alien", scene9, 10, 5,
+            "Abaikan dan lanjut", scene9, 15, 0);
 
-        Scene alienShipScene = new Scene("Kamu melihat kapal alien!", 
-            "Coba berkomunikasi", asteroidScene, 0, 10, 
-            "Serang", asteroidScene, 20, 5, 
-            "Kabur", asteroidScene, 10, 0
-        );
+        Scene scene7 = new Scene("Sinyal misterius terdeteksi.",
+            "Ikuti sinyal", scene8, 5, 10,
+            "Abaikan sinyal", scene8, 0, 0,
+            "Lacak sumbernya", scene8, 10, 5);
 
-        Scene battleScene = new Scene("Alien jahat menyerang!", 
-            "Lawan", alienShipScene, 20, 20, 
-            "Gunakan strategi", alienShipScene, 10, 15, 
-            "Kabur", alienShipScene, 5, 5
-        );
+        Scene scene6 = new Scene("Navigasi sistem bermasalah.",
+            "Perbaiki manual", scene7, 10, 10,
+            "Gunakan sistem backup", scene7, 0, 5,
+            "Abaikan", scene7, 15, 0);
 
-        Scene startScene = new Scene("Bumi hancur! Kamu harus pergi mencari planet baru.", 
-            "Pergi ke luar angkasa", battleScene, 0, 10, 
-            "Sembunyi di bunker", battleScene, 5, 5, 
-            "Lawan alien", battleScene, 20, 10
-        );
+        Scene scene5 = new Scene("Alien jahat menyerang!",
+            "Lawan", scene6, 20, 20,
+            "Gunakan strategi", scene6, 10, 15,
+            "Kabur", scene6, 5, 5);
 
-        Story story = new Story(startScene, player);
+        Scene scene4 = new Scene("Kamu melihat kapal alien!",
+            "Coba berkomunikasi", scene5, 0, 10,
+            "Serang", scene5, 20, 5,
+            "Kabur", scene5, 10, 0);
+
+        Scene scene3 = new Scene("Hujan asteroid mendekat!",
+            "Hindari dengan kecepatan tinggi", scene4, 5, 10,
+            "Gunakan perisai", scene4, 0, 5,
+            "Terobos", scene4, 15, 0);
+
+        Scene scene2 = new Scene("Kamu hampir tertarik ke lubang hitam!",
+            "Gunakan mesin dorong", scene3, 5, 10,
+            "Gunakan gravitasi", scene3, 10, 15,
+            "Abaikan peringatan", scene3, 20, 5);
+
+        Scene scene1 = new Scene("Bumi hancur! Kamu harus pergi mencari planet baru.",
+            "Pergi ke luar angkasa", scene2, 0, 10,
+            "Sembunyi di bunker", scene2, 5, 5,
+            "Lawan alien", scene2, 20, 10);
+
+        Story story = new Story(scene1, player);
         story.start();
     }
 }
